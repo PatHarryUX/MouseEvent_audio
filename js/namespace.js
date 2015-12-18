db9cd1e24677ba016a781f789bfa85ee8ba6d90e
Pat <patharryux@gmail.com>
2015-12-18 14:17:07 -0800

    Add audio controller

--- js/namespace.js
+++ js/namespace.js
@@ -5,6 +5,7 @@ App.Collections || (App.Collections = {});
 App.Router      || (App.Router = {});
 App.Views       || (App.Views = {});
 App.Templates   || (App.Templates = {});
+App.Audio       || (App.Audio = {});
 
 // Application instance.
 var app = app   || {};
